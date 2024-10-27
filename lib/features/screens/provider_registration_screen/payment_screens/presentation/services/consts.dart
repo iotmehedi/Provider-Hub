ababc4277202bb8ffe314c1fa8ec7983bbe998b5
Neways <neways3snit@gmail.com>
2024-10-27 20:14:19 +0600

    some changes. now complete service wise provider show

---
+++ lib/features/screens/provider_registration_screen/payment_screens/presentation/services/consts.dart
@@ -0,0 +1,8 @@
+const String stripePublishableKey =
+    "";
+const String stripeSecretKey =
+    "";
+
+
+// const String stripePublishableKey = "pk_live_51Pp0mcP21Q2jYuZBns9sl58GpXHSYMMVowRZ4KDb00t6slwrINIYsDGFVZTEVtY5r6iNttftUE6dtIvBzcM9g0vz0026EVZCfF";
+// const String stripeSecretKey = "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";
